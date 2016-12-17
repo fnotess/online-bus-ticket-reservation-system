8c803841b473f7b8ba8543a654ff0f078a11c6f8
fnote <sithijarox@yahoo.com>
2016-12-17 17:44:12 +0530

    minor change conductor page

--- resources/views/conductorPage.blade.php
+++ resources/views/conductorPage.blade.php
@@ -57,18 +57,7 @@
 
 
 
-    <div class="row">
-        <div class="col-lg-6">
-            <div class="input-group">
-      <span class="input-group-btn">
-        <button class="btn btn-default" type="button">Go!</button>
-      </span>
-                <input type="text" class="form-control" placeholder="Search for...">
-            </div><!-- /input-group -->
-        </div><!-- /.col-lg-6 -->
-
-    </div><!-- /.row --
-
+    
 
 
 
